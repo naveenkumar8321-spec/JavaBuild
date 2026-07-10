b157e1b758588c7a7dc15be32ded47b5256d8a00
naveenkumar8321-spec <naveenkumar8321@gmail.com>
2026-07-10 09:29:35 +0530

    July10

---
+++ src/MainPackage/ReverseString.java
@@ -0,0 +1,16 @@
+package MainPackage;
+
+public class ReverseString {
+
+    public static void main (String args[])
+    {
+        String Name = "naveen";
+
+        for(int i=Name.length()-1;i>=0;i--)
+        {
+            System.out.println(Name.charAt(i));
+        }
+    }
+
+
+}
